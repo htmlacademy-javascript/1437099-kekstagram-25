1b803249c1a9710033f593d778943d0b2ace3850
Vilenan <vilenan@mail.ru>
2022-03-06 08:38:09 +0300

    устранение замечаний eslint

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ function getRandomInt(a, b) {
   const from = Math.ceil(Math.min(a, b));
   const to = Math.floor(Math.max(a, b));
   return Math.floor(Math.random() * (to - from + 1) + from);
-};
-
+}
 getRandomInt(5, 8);
-
 function checkStringLength(string, length) {
   return string.length <= length;
 }
