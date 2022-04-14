daa73716d8251c1b5cb16cefa869bf175c74542b
Vilenan <vilenan@mail.ru>
2022-04-14 22:30:52 +0300

    валидация хэштегов 3

--- js/form.js
+++ js/form.js
@@ -10,19 +10,22 @@ closeFormModalElement.addEventListener('click', ()=>{
 const form = document.querySelector('.img-upload__form');
 const pristine = new Pristine(form);
 
+const hashtagInput = form.querySelector('.text__hashtags');
+pristine.addValidator(hashtagInput, (value) => {
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  const hastagInputArray = value.split(' ');
+  console.log(hastagInputArray);
+  if (hastagInputArray.length <= 5 && hastagInputArray.every( (e, i, a) => a.indexOf(e) === i ) && hastagInputArray.forEach((element) => re.test(element))){
+    return true;
+  }
+  return false;
+},  'errorMessage', 2, false);
+
 form.addEventListener('submit',(evt)=>{
   evt.preventDefault();
   const isValid = pristine.validate();
   if (isValid) {
-    const hashtagInput = form.querySelector('.text__hashtags');
-    pristine.addValidator(hashtagInput, (value) => {
-      const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
-      const hastagInputArray = value.split(' ');
-      if (hastagInputArray.length <= 5 && hastagInputArray.every( (e, i, a) => a.indexOf(e) === i ) && hastagInputArray.forEach((el)=>re.test(el))){
-        return true;
-      }
-      return false;
-    },  'errorMessage', 2, false);
+    console.log('можно отправлять');
   } else {
     console.log('нельзя отправлять');
   }
